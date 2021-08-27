ae1008d859322775d448186710cc4754e597061f
Tjitse Efdé <tjitse@vendic.nl>
2021-08-27 09:32:29 +0200

    Add remove assignee flag

--- __tests__/change_assignees.test.ts
+++ __tests__/change_assignees.test.ts
@@ -4,13 +4,13 @@ import nock from "nock";
 import {expect, test} from '@jest/globals'
 import change_assignees from '../src/change_assignees'
 
-test('Assigns the task to Bar and Baz, removes user Foo', async () => {
+/**
+ * The default mode does an API request to Clickup first, to GET the current assignees. Then it compares the current
+ * assignees with the target assignees.
+ */
+test('Default mode, assigns the task to Bar and Baz, removes user Foo', async () => {
     // Settings
-    process.env['INPUT_CLICKUP_TOKEN'] = 'pk_123'
-    process.env['INPUT_CLICKUP_CUSTOM_TASK_IDS'] = 'MAX-185'
-    process.env['INPUT_CLICKUP_TEAM_ID'] = '123'
-    process.env['INPUT_TARGET_ASSIGNEES_USERNAMES'] = 'bar\nbaz\nsome-random-user'
-    process.env['INPUT_CLICKUP_USER_ID_MAPPING_PATH'] = __dirname + '/' + 'user_mapping.json'
+    process.env['INPUT_ADD_ASSIGNEES_MODE'] = 'true'
 
     // Mocks
     const infoMock = jest.spyOn(core, 'info')
@@ -47,4 +47,38 @@ test('Assigns the task to Bar and Baz, removes user Foo', async () => {
     expect(infoMock).toHaveBeenCalledWith('MAX-185 assigned to Bar')
     expect(infoMock).toHaveBeenCalledWith('MAX-185 assigned to Baz')
     expect(infoMock).toHaveBeenCalledTimes(2)
+})
+
+/**
+ * The add assignee mode is set to false here, we won't compare the current users, we will just send an API request
+ * to Clickup that removes specified users.
+ */
+test('Remove mode, unassigns the users Baz and Bar, keeps Foo', async () => {
+    // Settings
+    process.env['INPUT_ADD_ASSIGNEES_MODE'] = 'false'
+
+    const debugMock = jest.spyOn(core, 'debug')
+    const failedMock = jest.spyOn(core, 'setFailed')
+    const body = {
+        "assignees": {
+            "add": [],
+            "rem": [
+                2,
+                3
+            ]
+        }
+    }
+
+    await change_assignees()
+
+    expect(debugMock).toHaveBeenCalledWith(JSON.stringify(body))
+    expect(failedMock).toHaveBeenCalledTimes(0)
+})
+
+beforeEach(() => {
+    process.env['INPUT_CLICKUP_TOKEN'] = 'pk_123'
+    process.env['INPUT_CLICKUP_CUSTOM_TASK_IDS'] = 'MAX-185'
+    process.env['INPUT_CLICKUP_TEAM_ID'] = '123'
+    process.env['INPUT_TARGET_ASSIGNEES_USERNAMES'] = 'bar\nbaz\nsome-random-user'
+    process.env['INPUT_CLICKUP_USER_ID_MAPPING_PATH'] = __dirname + '/' + 'user_mapping.json'
 })
--- src/change_assignees.ts
+++ src/change_assignees.ts
@@ -1,82 +1,33 @@
 import * as core from '@actions/core'
-import axios from 'axios'
 import * as fs from "fs";
-
-function getTargetAssignees(target_assignees_usernames: string[], clickup_user_id_mapping: any) : number[] {
-    const target_assignees = target_assignees_usernames.map(
-        username => clickup_user_id_mapping[username.toString()]
-    ).filter(id => id !== undefined);
-    // Force return as ints, the Clickup API expects integers only.
-    return target_assignees.map((target_assignee) => {
-        return parseInt(target_assignee)
-    })
-}
+import {addAssignees, removeAssignees} from './api_calls'
 
 export default async function change_assignees(): Promise<void> {
     try {
         let failed: boolean = false;
         const token: string = core.getInput('clickup_token')
-        const task_ids : string[] = core.getMultilineInput('clickup_custom_task_ids')
-        const team_id : string = core.getInput('clickup_team_id')
-        const target_assignees_usernames : string[] = core.getMultilineInput('target_assignees_usernames')
-        const mapping_path : string = core.getInput('clickup_user_id_mapping_path')
-        const mapping_json : string = fs.readFileSync(mapping_path, 'utf-8')
-        const mapping : any = JSON.parse(mapping_json)
+        const task_ids: string[] = core.getMultilineInput('clickup_custom_task_ids')
+        const team_id: string = core.getInput('clickup_team_id')
+        const target_assignees_usernames: string[] = core.getMultilineInput('target_assignees_usernames')
+        const mapping_path: string = core.getInput('clickup_user_id_mapping_path')
+        const mapping_json: string = fs.readFileSync(mapping_path, 'utf-8')
+        const mapping: any = JSON.parse(mapping_json)
         const target_assignees: number[] = getTargetAssignees(target_assignees_usernames, mapping)
+        const add_assignees : boolean = core.getBooleanInput('add_assignees_mode')
 
         for (const task_id of task_ids) {
-            let endpoint = `https://api.clickup.com/api/v2/task/${task_id}/?custom_task_ids=true&team_id=${team_id}`
-            let result = await axios.get(endpoint, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': token
+            try {
+                if (add_assignees) {
+                    await addAssignees(task_id, team_id, token, target_assignees)
+                } else {
+                    await removeAssignees(task_id, team_id, token, target_assignees)
                 }
-            })
-
-            core.debug(`GET request for ${task_id} output:`)
-            core.debug(JSON.stringify(result.data))
-
-            let current_assignees : number[] = result.data.assignees.map((elem: { id: number; }) => (elem.id)) ?? []
-            let remove_assignees : number[] = current_assignees.filter(x => !target_assignees.includes(x)) ?? []
-            let add_assignees : number[] = target_assignees.filter(x => !current_assignees.includes(x)) ?? []
-
-            core.debug(`Current assignees: ${current_assignees.join(',')}`)
-            core.debug(`Remove assignees: ${remove_assignees.join(',')}`)
-            core.debug(`Add assignees: ${add_assignees.join(',')}`)
-
-            let body = {
-                "assignees": {
-                    "add": [
-                        ...add_assignees
-                    ],
-                    "rem": [
-                        ...remove_assignees
-                    ]
-                }
+            } catch (error) {
+                failed = true
+                core.info(`${task_id} error: ${error.message}`)
+                core.debug(`Error output for ${task_id}`)
+                core.debug(JSON.stringify(error))
             }
-
-            core.debug(`Put request for ${task_id}:`)
-            core.debug(JSON.stringify(body))
-
-            await axios.put(endpoint, body, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': token
-                }
-            }).then(
-                (result) => {
-                    result.data.assignees.forEach((assignee : any) => core.info(`${task_id} assigned to ${assignee.username}`))
-                    core.debug('Put request response:')
-                    core.debug(JSON.stringify(result.data))
-                }
-            ).catch(
-                function (error) {
-                    failed = true
-                    core.info(`${task_id} error: ${error.message}`)
-                    core.debug(`Error output for ${task_id}`)
-                    core.debug(JSON.stringify(error))
-                }
-            )
         }
 
         if (failed) {
@@ -86,4 +37,14 @@ export default async function change_assignees(): Promise<void> {
     } catch (error) {
         core.setFailed(`Action failed: ${error}`)
     }
+}
+
+function getTargetAssignees(target_assignees_usernames: string[], clickup_user_id_mapping: any): number[] {
+    const target_assignees = target_assignees_usernames.map(
+        username => clickup_user_id_mapping[username.toString()]
+    ).filter(id => id !== undefined);
+    // Force return as ints, the Clickup API expects integers only.
+    return target_assignees.map((target_assignee) => {
+        return parseInt(target_assignee)
+    })
 }
